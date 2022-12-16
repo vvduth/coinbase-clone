8c97264512fc7a2cbf286450b47bea7d1e60b13f
Duc Thai <ducthai060501@gmail.com>
2022-12-16 14:56:53 +0200

    render list with map method

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ListItem from './components/ListItem';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ListItem from "./components/ListItem";
+import { SAMPLE_DATA } from "./assets/dummyData/dummyData";
 export default function App() {
+  if (SAMPLE_DATA) {
+    console.log(SAMPLE_DATA.length);
+  }
   return (
     <View style={styles.container}>
-      <View style = {styles.titleWrapper}>
+      <View style={styles.titleWrapper}>
         <Text style={styles.largeTitle}>Markets</Text>
       </View>
-      <View style ={styles.divider}/>
-      <ListItem />
+      <View style={styles.divider} />
+
+      {SAMPLE_DATA.map((coin) => (
+        <ListItem
+          name={coin.name}
+          key={coin.id}
+          symbol={coin.symbol}
+          currentPrice={coin.current_price}
+          pricePercentage7d={coin.price_change_percentage_7d_in_currency}
+          logoUrl={coin.image}
+        />
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,20 +31,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   titleWrapper: {
     marginTop: 80,
-    paddingHorizontal: 16 , 
+    paddingHorizontal: 16,
   },
   divider: {
-    height: StyleSheet.hairlineWidth ,
-    backgroundColor: '#A9ABB1',
-    marginHorizontal: 16, 
-    marginTop: 16, 
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
   },
   largeTitle: {
-    fontSize: 24, 
-    fontWeight: "bold" , 
-  }
+    fontSize: 24,
+    fontWeight: "bold",
+  },
 });
--- components/ListItem.js
+++ components/ListItem.js
@@ -2,18 +2,19 @@ import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
 const ListItem = ({name, symbol, currentPrice, pricePercentage7d, logoUrl }) => {
-  return (
+    console.log("Name  ", name )
+    return (
     <TouchableOpacity>
       <View style={styles.itemWrapper}>
         {/* left side */}
         <View style ={styles.leftWrapper}>
-            <Image style= {styles.image} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Eth-diamond-rainbow.png'}}/>
+            <Image style= {styles.image} source={{uri: logoUrl}}/>
             <View style={styles.titlesWrapper}>
                 <Text style={styles.title}>
-                    Etherum
+                    {name}
                 </Text>
                 <Text style={styles.subTitle}>
-                    ETH
+                    {symbol}
                 </Text>
             </View>
         </View>
@@ -21,10 +22,10 @@ const ListItem = ({name, symbol, currentPrice, pricePercentage7d, logoUrl }) => 
         {/* right side */}
         <View styles ={styles.rightWrapper}>
                 <Text style={styles.title}>
-                    1200
+                    {currentPrice}
                 </Text>
                 <Text style={[styles.subTitle, {color: 'red'}]}>
-                    ETH
+                    {pricePercentage7d}
                 </Text>
         </View>
 
